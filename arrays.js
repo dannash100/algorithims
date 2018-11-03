a0da458784a08d4fa693b09ab229619903731209
dannash100 <danster15@gmail.com>
2018-11-03 16:40:29 +1300

    first two questions

--- arrays.js
+++ arrays.js
@@ -3,15 +3,20 @@ What if you cannot use additional data structures? */
 
 const isUnique = (string) => {
   for (const character of string.replace(/\s/g, '')) {
-    const occurrences = string.split(character).length - 1;
-    if (occurrences > 1) return false;
+    const occurrences = string.split(character).length - 1
+    if (occurrences > 1) return false
   }
-  return true;
-};
+  return true
+}
 
+/* Check Permutation: Given two strings,write a method to decide if
+ one is a permutation of the other. */
 
-// Check Permutation: Given two strings,write a method to decide if one is a permutation of the other.
+const sortString = string => string.split('').sort().join()
 
-const sortString = string => string.split('').sort().join();
+const checkPermutation = (string, string2) => sortString(string) === sortString(string2)
 
-const checkPermutation = (string, string2) => sortString(string) === sortString(string2);
+module.exports = {
+  isUnique,
+  checkPermutation
+}
