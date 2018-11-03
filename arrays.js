93668e5a2a02db2e7e99b8dc07728531f32a660c
dannash100 <danster15@gmail.com>
2018-11-03 16:31:15 +1300

    setup

---
+++ arrays.js
@@ -0,0 +1,17 @@
+/* Is Unique: Implement an algorithm to determine if a string has all unique characters.
+What if you cannot use additional data structures? */
+
+const isUnique = (string) => {
+  for (const character of string.replace(/\s/g, '')) {
+    const occurrences = string.split(character).length - 1;
+    if (occurrences > 1) return false;
+  }
+  return true;
+};
+
+
+// Check Permutation: Given two strings,write a method to decide if one is a permutation of the other.
+
+const sortString = string => string.split('').sort().join();
+
+const checkPermutation = (string, string2) => sortString(string) === sortString(string2);
